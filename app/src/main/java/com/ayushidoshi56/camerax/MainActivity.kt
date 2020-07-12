d483e39db92aa89525e941324747a63415d7737f
legendarygirl56 <ayushidoshi56@gmail.com>
2020-07-12 17:25:55 +0530

    camera access

---
+++ app/src/main/java/com/ayushidoshi56/camerax/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.ayushidoshi56.camerax
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+open class MainActivity : AppCompatActivity() {
+
+    val CAMERA_REQUEST_CODE = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+            iv.setOnClickListener {
+                val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                if (callCameraIntent.resolveActivity(packageManager) != null) {
+                    startActivityForResult(callCameraIntent, CAMERA_REQUEST_CODE)
+                }
+            }
+        }
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            when (requestCode) {
+                CAMERA_REQUEST_CODE -> {
+                    if (resultCode == Activity.RESULT_OK && data != null) {
+                        iv.setImageBitmap(data.extras?.get("data") as Bitmap)
+
+                    }
+                }
+                else -> {
+                    Toast.makeText(this, "Unrecognized request code", Toast.LENGTH_SHORT)
+                }
+            }
+        }
+}
